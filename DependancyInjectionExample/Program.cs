41f62aa7fe8756d474722eac9e0965afb9afa4d3
Stephan Marais <stephan.marais@entelect.co.za>
2018-03-01 07:31:42 +0200

    initial commit

---
+++ DependancyInjectionExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DependancyInjectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

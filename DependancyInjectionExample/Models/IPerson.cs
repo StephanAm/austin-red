c658106e05c4b8e29fdd54dc14f28ce69d8b2442
Stephan Marais <stephan.marais@entelect.co.za>
2018-03-01 08:09:26 +0200

    Add person model

---
+++ DependancyInjectionExample/Models/IPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DependancyInjectionExample.Models
+{
+    public enum Gender
+    {
+        male = 1,
+        female
+    }
+
+    interface IPerson
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+        string Surname { get; set; }
+        UInt16 Age { get; set; }
+    }
+}
